341a5353d53673344209e7be6184bc012112f963
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-09-24 20:03:11 -0300

    obsolote IDE targets cleanup, moved src-deprecated folder to deprecated

--- src/ide/core/xenoide-win32xx/src/documents_view.cpp
+++
@@ -1,8 +0,0 @@
-#include "documents_view.h"
-
-DocumentsView::DocumentsView() {
-    SetTabText(L"Untitled");
-    SetDockCaption(L"Documents");
-    SetHideSingleTab(TRUE);
-    SetView(m_editor);
-}
