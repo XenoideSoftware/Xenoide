e54640a4b1252ed0a9e338690de45b6441ef5d2f
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-09-24 13:15:00 -0300

    renamed conan mise tasks to better name them, fixed issue where some recipes where not exported on Windows

--- mise/export-recipes.ps1
+++ mise/export-recipes.ps1
@@ -8,7 +8,52 @@ Get-ChildItem -Path $packagesDir -Directory | Sort-Object Name | ForEach-Object 
     $conanfile = Join-Path $pkgDir "conanfile.py"
     $conandata = Join-Path $pkgDir "conandata.yml"
 
+    $config = Join-Path $pkgDir "config.yml"
+
     if (-not (Test-Path $conanfile)) {
+        if (Test-Path $config) {
+            $versions = @()
+            $folders = @()
+            $inVersions = $false
+            $version = ""
+            Get-Content $config | ForEach-Object {
+                $line = $_
+                $stripped = $line.Trim()
+                if ($stripped -match '^versions:') {
+                    $inVersions = $true
+                    return
+                }
+                if ($inVersions) {
+                    if ($line -match '^\s{2}["`'']?[^"''\s:]+["`'']?\s*:\s*$') {
+                        if ($line -match "^\s*['`"']?([^'''':]+)['`"']?\s*:\s*$") {
+                            $version = $matches[1]
+                        }
+                    } elseif ($version) {
+                        if ($line -match '^\s*folder:\s*["`'']?([^"`'']+)["`'']?\s*$') {
+                            $folder = $matches[1]
+                            if ($folder) {
+                                $versions += $version
+                                $folders += $folder
+                                $version = ""
+                            }
+                        }
+                    }
+                    if ($stripped -and $line -notmatch '^\s' -and $stripped -notmatch '^#') {
+                        $inVersions = $false
+                    }
+                }
+            }
+
+            if ($versions.Count -gt 0) {
+                for ($i = 0; $i -lt $versions.Count; $i++) {
+                    $v = $versions[$i]
+                    $folder = $folders[$i]
+                    Write-Host "==> Exporting $pkg/$v ($pkgDir/$folder)"
+                    conan export "$pkgDir/$folder" --version $v
+                }
+                return
+            }
+        }
         return
     }
 
