c8f415d00f17788d8e804531c36f41972125d657
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-09-23 23:20:37 -0300

    modularized the engine,  re-enabled gltfc, gltf-viewer and ktxc. Now imgui backends are copied dynamically based on current dependencies

--- conanfile.py
+++ conanfile.py
@@ -22,15 +22,25 @@ class xenoideRecipe(ConanFile):
     options = {
         "with_ide": [True, False],
         "with_ide_winlamb": [True, False],
-        "with_engine": [True, False],
         "with_tests": [True, False],
+        "with_engine": [True, False],
+        "with_engine_renderer_gl": [True, False],
+        "with_engine_renderer_vk": [True, False],
+        "with_engine_tool_ktxc": [True, False],
+        "with_engine_tool_gltfc": [True, False],
+        "with_engine_tool_gltfv": [True, False],
     }
 
     default_options = {
         "with_ide": True,
         "with_ide_winlamb": True,
-        "with_engine": True,
         "with_tests": True,
+        "with_engine": True,
+        "with_engine_renderer_gl": True,
+        "with_engine_renderer_vk": True,
+        "with_engine_tool_ktxc": True,
+        "with_engine_tool_gltfc": True,
+        "with_engine_tool_gltfv": True,
     }
 
     # Sources are located in the same place as this recipe, copy them to the recipe
@@ -45,53 +55,76 @@ class xenoideRecipe(ConanFile):
                 print("with_ide_winlamb is enabled but target OS is not windows. Changing to False")
                 self.options.rm_safe("with_ide_winlamb")
 
+    def _require_cli(self):
+        # Add here additional options that might build a custom CLI tool
+        return self.options.get_safe("with_engine_tool_ktxc") or self.options.get_safe("with_engine_tool_gltfc")
+
     def requirements(self):        
         # required by all
-        if self.options.with_tests:
+        if self.options.with_tests and (self.options.get_safe("with_ide") or self.options.get_safe("with_engine")):
             self.requires("catch2/3.14.0")
 
-        # required by the ide
-        # TODO: Parametrize dependency target OS
-        if self.options.get_safe("with_ide_winlamb"):
-            self.requires("winlamb/2026.06.24")
-            self.requires("scintilla3/3.7.6")
+        if self.options.get_safe("with_ide"):
+            # required by the ide
+            # TODO: Parametrize dependency target OS
+            if self.options.get_safe("with_ide_winlamb"):
+                self.requires("winlamb/2026.06.24")
+                self.requires("scintilla3/3.7.6")
+
+        if self._require_cli():
+            self.requires("cxxopts/3.3.1")
 
         # required by the engine
-        self.requires("fmt/[>=11 <12]")
-        self.requires("ms-gsl/4.2.0")
-        self.requires("glfw/3.4")
-        self.requires("glazed/1.0.0", options={"language": "both", "apis": "gl:4.6,gles2:3.2,gl_compat:2.1"})
-        self.requires("backport-cpp/1.2.0")
-        self.requires("tl-expected/1.2.0")
-        self.requires("gsl-lite/1.1.0")
+        if self.options.get_safe("with_engine"):
+            self.requires("fmt/[>=11 <12]")
+            self.requires("ms-gsl/4.2.0")
+            self.requires("backport-cpp/1.2.0")
+            self.requires("tl-expected/1.2.0")
+            self.requires("gsl-lite/1.1.0")
 
-        """
-        self.requires("imgui/1.92.2b")
+            if self.options.get_safe("with_engine_renderer_vk"):
+                self.requires("vulkan-loader/1.4.313.0")
 
-        # self.requires("assimp/6.0.2")
-        
-        # self.requires("cgltf/1.13")
-        # self.requires("lodepng/cci.20230410")
-        # self.requires("devil/1.8.0")
-        
-        # self.requires("nlohmann_json/3.12.0")
-        # self.requires("vulkan-loader/1.4.313.0")
-        """
-        
-        """
-        compiler = self.settings.get_safe("compiler")
-        compiler_version = self.settings.get_safe("compiler.version")
-        if not (compiler == "gcc" and str(compiler_version).startswith("5")):
-            self.requires("ktx/4.4.2")
-        """
+            if self.options.get_safe("with_engine_renderer_gl"):
+                self.requires("glfw/3.4")
+                self.requires("glazed/1.0.0", options={"language": "both", "apis": "gl:4.6,gles2:3.2,gl_compat:2.1"})
 
-        """
-        self.requires("cxxopts/3.3.1")
-        # NOTE: sdl2 and sdl3 both don't build under ArchLinux
-        # Neither with gcc-x86 (gcc5)
-        self.requires("sdl/2.32.10")
-        """
-        
+            if self.options.get_safe("with_engine_tool_ktxc"):
+                self.requires("ktx/4.4.2")
+                self.requires("devil/1.8.0")
+
+            if self.options.get_safe("with_engine_tool_gltfc"):
+                self.requires("assimp/6.0.2")
+                self.requires("cgltf/1.13")
+
+            if self.options.get_safe("with_engine_tool_gltfv"):
+                self.requires("imgui/1.92.2b")
+                self.requires("sdl/2.32.10")
+
+            """
+            # self.requires("lodepng/cci.20230410")
+            # self.requires("nlohmann_json/3.12.0")
+            """
+
+    def _enumerate_imgui_backends(self):
+        # TODO: Check specific package versions to pick correct imgui backend version
+
+        if "imgui" not in self.dependencies:
+            return []
+
+        backends = []
+
+        if "glazed" in self.dependencies:
+            backends.append("opengl3")
+
+        if "glfw" in self.dependencies:
+            backends.append("glfw")
+
+        if "sdl" in self.dependencies:
+            backends.append("sdl2")
+
+        return backends
+
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
@@ -102,18 +135,23 @@ class xenoideRecipe(ConanFile):
         tc.variables["XE_ENABLE_ENGINE"] = "ON" if self.options.with_engine else "OFF"
         tc.variables["XE_ENABLE_TESTING"] = "ON" if self.options.with_tests else "OFF"
         tc.variables["XE_ENABLE_IDE_WINLAMB"] = "ON" if self.options.get_safe("with_ide_winlamb") else "OFF"
+        tc.variables["XE_ENABLE_ENGINE_TOOL_KTXC"] = "ON" if self.options.get_safe("with_engine_tool_ktxc") else "OFF"
+        tc.variables["XE_ENABLE_ENGINE_TOOL_GLTFC"] = "ON" if self.options.get_safe("with_engine_tool_gltfc") else "OFF"
+        tc.variables["XE_ENABLE_ENGINE_TOOL_GLTFV"] = "ON" if self.options.get_safe("with_engine_tool_gltfv") else "OFF"
+
+        # populate imgui backends
+        imgui_backends = self._enumerate_imgui_backends()
+        if len(imgui_backends) > 0:
+            package_folder = self.dependencies["imgui"].package_folder
+            bindings_folder_src = os.path.join(package_folder, "res", "bindings")
+            bindings_folder_dest = os.path.join(self.source_folder, "src", "engine", "tools", "libxe-imgui", "src")
+
+            for backend in imgui_backends:
+                copy(self, f"imgui_impl_{backend}.*", bindings_folder_src, bindings_folder_dest)
+
+            tc.variables["XE_ENGINE_IMGUI_BACKENDS"] = ";".join(imgui_backends)
+
         tc.generate()
-
-        """
-        # imgui backends
-        package_folder = self.dependencies["imgui"].package_folder
-        bindings_folder_src = os.path.join(package_folder, "res", "bindings")
-        bindings_folder_dest = os.path.join(self.source_folder, "src", "xe-gltf-view", "src", "bindings")
-
-        backends = ["glfw", "opengl3", "sdl2", "sdl3"]
-        for backend in backends:
-            copy(self, f"*{backend}*", bindings_folder_src, bindings_folder_dest)
-        """
 
     def build(self):
         cmake = CMake(self)
